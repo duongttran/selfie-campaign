ef9158a550c132a24f609d2d3840470d519d9799
Duong Tran <duongtran@Duongs-MacBook-Pro.local>
2022-05-05 23:51:29 -0700

    fix show/hide box

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let checkBox = document.getElementById("checkbox");
 
 let trialBox = document.getElementsByClassName("trial-block")[0]
 
+let hideBox = document.getElementsByClassName("hideBox")[0]
+
+hideBox.style.display = "none"
+
 function tickBox() {
     console.log("tickkkkk the boxxxxx")
 
@@ -20,5 +24,6 @@ function submitSelfie() {
     if (confirm("Are you sure you want to submit?") == true) {
         trialBox.style.display = "none"
         console.log("confirm giup minh")
+        hideBox.style.display = "block"
     }
 }
