c930e2823efff379f1d54bcc79fc9f6c00b9e1e1
Duong Tran <duongtran@Duongs-MacBook-Pro.local>
2022-05-05 22:55:57 -0700

    add animations and js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("sdasdsad")
+let submitBtn = document.getElementById("submitBtn");
+
+let checkBox = document.getElementById("checkbox");
+
+let trialBox = document.getElementsByClassName("trial-block")[0]
+
+function tickBox() {
+    console.log("heyeerera")
+
+    if (checkBox.checked === true) {
+        submitBtn.classList.remove("disabled")
+    } else {
+        submitBtn.classList.add("disabled")
+        submitBtn.disabled = true;
+
+    }
+}
+
+function submitSelfie() {
+    if (confirm("are u sure to submit?") == true) {
+        trialBox.style.display = "none"
+        console.log("confirm giup minh")
+    }
+}
